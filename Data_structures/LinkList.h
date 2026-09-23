2b146ff71b48a2736a45432691047f8c4f0da679
Lê Hữu Hoàng <hoanglehuu26@gmail.com>
2026-09-23 10:02:57 +0700

    ..

--- Data_structures/LinkList.h
+++ Data_structures/LinkList.h
@@ -1,12 +1,8 @@
 #ifndef LINKLIST_H
 #define LINKLIST_H
 
-#include "Doctor.h"
-#include "Appointment.h"
-#include "AuthController.h"
 
-template <typename T>
-
+template <class T>
 class LinkList {
     public:
         struct Node {
@@ -18,15 +14,77 @@ class LinkList {
         Node *header;
         Node *footer;
     public:
-        LinkList();
-        LinkList(const LinkList &Ll);
-        ~LinkList();
+        LinkList(): header(new Node), footer(new Node)  {
+            header->next = footer;
+            header->prev = nullptr;
 
-        bool insert(const T &info);
-        bool remove(LinkList::Node *N);
+            footer->next = nullptr;
+            footer->prev = header;
+        }
 
-        Node *getHeader() const;
-        Node *getFooter() const;
+        LinkList(const LinkList &Ll): header(new Node), footer(new Node) {
+            header->next = footer;
+            header->prev = nullptr;
+
+            footer->next = nullptr;
+            footer->prev = header;
+
+            Node *tmp = L.header->next;
+            while (tmp->next != nullptr) {
+                Node *tmpNode = new Node;
+                tmpNode->info = tmp->info;
+
+                tmpNode->next = footer;
+                tmpNode->prev = footer->prev;
+                if (footer->prev != nullptr)
+                    footer->prev->next = tmpNode;
+                footer->prev = tmpNode;
+
+                tmp = tmp->next;
+            }
+        }
+        ~LinkList() {
+            Node *tmp = header;
+            while (tmp != nullptr) {
+                Node *tmptotmp = tmp->next;
+                delete tmp;
+
+                tmp = tmptotmp;
+            }
+        }
+
+        bool insert(const T &info) {
+            Node *tmp = new Node;
+            if (tmp == nullptr)
+                return false;
+
+            tmp->info = info;
+            tmp->next = header->next;
+            tmp->prev = header;
+            if (header->next != nullptr)
+                header->next->prev = tmp;
+            header->next = tmp;
+
+            return true;
+        }  
+
+        bool remove(Node *N) {
+            if (N == header || N == footer)
+                return false;
+
+            N->next->prev = N->prev;
+            N->prev->next = N->next;
+
+            delete N;
+            return true;
+        }
+
+        Node *getHeader() const {
+            return header;
+        }
+        Node *getFooter() const {
+            return footer;
+        }
 };
 
 #endif // LINKLIST_H
---
+++ Data_structures/PriorityQueue.h
@@ -0,0 +1,18 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+class priorityQueue {
+    public:
+        struct Node {
+            Node *p;
+            Node *l;
+            Node *r;
+
+            
+        }
+    private:
+
+        
+};
+
+#endif
